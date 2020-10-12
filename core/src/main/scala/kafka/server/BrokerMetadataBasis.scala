01011b034474bf5f646c9574bd87e08e76721bb4
Ron Dagostino <rdagostino@confluent.io>
2020-10-12 14:13:24 -0400

    additional docs for clarity

--- core/src/main/scala/kafka/server/BrokerMetadataBasis.scala
+++ core/src/main/scala/kafka/server/BrokerMetadataBasis.scala
@@ -19,22 +19,56 @@ package kafka.server
 
 import org.apache.kafka.common.protocol.ApiMessage
 
+/**
+ * There is no single place where all broker metadata lives.  For example, MetadataCache holds some of it,
+ * ReplicaManager holds some of it, etc.  This class is a composite of the pieces from the various locations and
+ * stitches them together into a single value.  Each individual piece is itself a {@link WriteableBasis} and
+ * can change independently of any other piece.
+ *
+ * @param metadataCacheBasis the MetadataCacheBasis defining part of the value
+ */
 case class BrokerMetadataValue(metadataCacheBasis: MetadataCacheBasis) {
   def newValue(metadataCacheBasis: MetadataCacheBasis): BrokerMetadataValue = {
+    // keep all parts except the one given
     BrokerMetadataValue(metadataCacheBasis)
+  }
+
+  /**
+   * For each underlying basis, write it if necessary
+   */
+  def writeIfNecessary() : Unit = {
+    metadataCacheBasis.writeIfNecessary()
   }
 }
 
-class BrokerMetadataHolder(metadataCache: MetadataCache) extends ValueHolder[BrokerMetadataValue] {
+/**
+ * There is no single place where all broker metadata lives.  For example, MetadataCache holds some of it,
+ * ReplicaManager holds some of it, etc.  This class can retrieve all of the pieces from the various locations
+ * and stitch them together into a single BrokerMetadataValue, and it can take such a value and push its pieces out
+ * to all the necessary destinations.  Each individual piece is itself a {@link WriteableBasis} and
+ * can change independently of any other piece.
+ *
+ * @param metadataCache the MetadataCache from which part of the value is retrieved/written
+ */
+private class BrokerMetadataHolder(metadataCache: MetadataCache) extends ValueHolder[BrokerMetadataValue] {
   override def retrieveValue(): BrokerMetadataValue = {
     BrokerMetadataValue(new MetadataCacheBasis(metadataCache))
   }
 
   override def writeValue(value: BrokerMetadataValue): Unit = {
-    value.metadataCacheBasis.writeIfNecessary()
+    value.writeIfNecessary()
   }
 }
 
+/**
+ * There is no single place where all broker metadata lives.  For example, MetadataCache holds some of it,
+ * ReplicaManager holds some of it, etc.  This class stitches the various pieces together and presents them as a single
+ * {@link WriteableBasis}.  It is itself a composite of individual {@link WriteableBasis} instances, each of which
+ * can change independently of any other piece, so it provides methods to create a new basis from the current one
+ * with changes to an underlying basis.
+ *
+ * @param metadataCache the MetadataCache that defines one underlying basis from which this one is constructed.
+ */
 class BrokerMetadataBasis(metadataCache: MetadataCache)
   extends WriteableBasis[BrokerMetadataValue](new BrokerMetadataHolder(metadataCache)) {
 
